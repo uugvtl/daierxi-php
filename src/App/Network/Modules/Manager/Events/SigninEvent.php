e06613467cdd2600c42f4588a47edcf9bddb3b2d
Leon <uugv_tl@163.com>
2018-01-05 01:17:34 +0800

    完成登入后台功能

--- src/App/Network/Modules/Manager/Events/SigninEvent.php
+++ src/App/Network/Modules/Manager/Events/SigninEvent.php
@@ -1,12 +1,6 @@
 <?php
 namespace App\Network\Modules\Manager\Events;
-use App\Frames\FrameClass;
-use App\Helpers\CookiesHelper;
-use App\Helpers\ErrorsHelper;
-use App\Helpers\InstanceHelper;
-use App\Network\Modules\ModuleController;
-use App\Network\Providers\NetworkContainerProvider;
-use Phalcon\Mvc\Dispatcher;
+use App\Globals\Events\AppSigninEvent;
 /**
  * Created by PhpStorm.
  * User: leon
@@ -16,87 +10,4 @@ use Phalcon\Mvc\Dispatcher;
  * Class SigninEvent
  * @package App\Network\Modules\Manager\Events
  */
-class SigninEvent extends FrameClass
-{
-
-    /**
-     * @var ModuleController
-     */
-    private $ctrl;
-
-    /**
-     * 控制器名称
-     * @var string
-     */
-    private $distributerCtrlName;
-
-    final public function init(...$args)
-    {
-
-        $ctrl = $args[0];
-
-        if(!$ctrl instanceof ModuleController)
-        {
-            $errorsHelper = ErrorsHelper::getInstance();
-            $errorsHelper->triggerError('init method only accepts Class ModuleController. Input was: '.$ctrl);
-        }
-
-        $this->ctrl = $ctrl;
-
-        return $this;
-    }
-
-    /**
-     * 设置控制器名称，
-     * @param string $ctrlString
-     * @return $this
-     */
-    final public function setDistributerCtrlName($ctrlString)
-    {
-        $this->distributerCtrlName = $ctrlString;
-        return $this;
-    }
-
-    /**
-     * 没有登入状态时，访问登入后的地址，
-     * @param Dispatcher $dispatcher        调度器实例
-     * @param string $providerClassString   Provider所在的模块类命名
-     * @return boolean
-     */
-    public function beforeExecuteRoute(Dispatcher $dispatcher, $providerClassString)
-    {
-        $toggle = true;
-        $cookiesHelper = CookiesHelper::getInstance();
-        $cookieValue = $cookiesHelper->setCookies($this->cookies)->getLoginCookie(LOGIN_MANAGER);
-        if($cookieValue)
-        {
-            $instanceHelper = InstanceHelper::getInstance();
-            $provider = $instanceHelper->build(NetworkContainerProvider::class, $providerClassString);
-            $distributer = $this->getCtrl()->createDistributer($dispatcher);
-            $distributer->setCtrlString($this->getDistributerCtrlName());
-            $provider->init($distributer);
-
-            $responder = $provider->setGeneralize(YES)->setPrefixString('Cookie')->getQueryResponder($cookieValue);
-            $toggle = $responder->toggle;
-        }
-
-        finish:
-        return $toggle;
-    }
-
-    /**
-     * @return ModuleController
-     */
-    final protected function getCtrl()
-    {
-        return $this->ctrl;
-    }
-
-    /**
-     * @return string
-     */
-    final protected function getDistributerCtrlName()
-    {
-        return $this->distributerCtrlName;
-    }
-}
+class SigninEvent extends AppSigninEvent{}
